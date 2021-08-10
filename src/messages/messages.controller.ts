5ea6f4bfcfd4fb47bb7b3d699d4f9be120f404e9
Theo <teo.pvh@gmail.com>
2021-08-10 13:27:55 -0400

    add update and delete endpoint

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { Message } from './Message';
 import { MessagesService } from './messages.service';
 
@@ -19,5 +27,15 @@ export class MessagesController {
   @Post()
   create(@Body() message: Message) {
     return this.messagesService.create(message);
+  }
+
+  @Put(':id')
+  update(@Param() params, @Body() message: Message) {
+    return this.messagesService.update(+params.id, message);
+  }
+
+  @Delete(':id')
+  delete(@Param() params) {
+    return this.messagesService.delete(+params.id);
   }
 }
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -28,4 +28,16 @@ export class MessagesService {
   create(message: Message) {
     return this.messages.push(message);
   }
+
+  update(id: number, message: Message) {
+    const index = this.messages.findIndex((message) => message.id === id);
+    this.messages[index] = message;
+    return message;
+  }
+
+  delete(id: number) {
+    const index = this.messages.findIndex((message) => message.id === id);
+    delete this.messages[index];
+    // return true;
+  }
 }
