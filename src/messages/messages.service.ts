767a863bff6417ea806c3a315d3584ee007fe16b
Theo <teo.pvh@gmail.com>
2021-08-10 13:36:58 -0400

    error

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -21,8 +21,12 @@ export class MessagesService {
     return this.messages;
   }
 
-  findById(id: number) {
-    return this.messages.find((message) => message.id === id);
+  async findById(id: number) {
+    const message = this.messages.find((msg) => message.id === id);
+    if (!message) {
+      throw Error(`Mensagem com o ID ${id} não encontrada`);
+    }
+    return message;
   }
 
   create(message: Message) {
