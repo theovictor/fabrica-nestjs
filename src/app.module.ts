9ffb5da03b034c552da20dffc34a0e12ec59e4fe
Theo <teo.pvh@gmail.com>
2021-08-12 10:47:03 -0400

    fix 'PrismaClient'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MessagesModule } from './messages/messages.module';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [PokemonModule],
   controllers: [AppController],
   providers: [AppService],
 })
